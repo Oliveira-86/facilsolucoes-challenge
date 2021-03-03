cd90445b0846a566f78b2967a2c5ffad8cd77d97
Egberto Oliveira <egberto.duarte.14@gmail.com>
2021-03-03 13:56:49 -0300

    Project created

---
+++ backend/src/main/java/com/facilsolucoes/challenge/FacilsolucoesApplication.java
@@ -0,0 +1,13 @@
+package com.facilsolucoes.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilsolucoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilsolucoesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/facilsolucoes/challenge/FacilsolucoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facilsolucoes.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacilsolucoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
